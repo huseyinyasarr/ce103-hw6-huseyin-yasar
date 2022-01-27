9dfa8a077334b97eddf50c995f0b90c304a23e21
huseyinyasarr <huseyin_yasar20@erdogan.edu.tr>
2022-01-27 11:43:56 +0300

    v2

---
+++ problem1/calculator_test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("calculator_test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("calculator_test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25c1a261-fb58-4d3b-8d67-a742886fca75")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ problem1/calculator_test/UnitTest1.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LesGraphingCalc;
+
+namespace ce103_hw6_calc_test_cs
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public void CalculateTest1()
+        {
+            var expected = RPN.Calculate("30+20");
+            var actual = 50;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest2()
+        {
+            var expected = RPN.Calculate("61-16");
+            var actual = 45;
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod()]
+        public void CalculateTest3()
+        {
+            var expected = RPN.Calculate("sqrt(25");
+            var actual = 5;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest4()
+        {
+            var expected = RPN.Calculate("ln(e^8");
+            var actual = 8;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest5()
+        {
+            var expected = RPN.Calculate("lg(1000");
+            var actual = 3;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest6()
+        {
+            var expected = RPN.Calculate(" %10");
+            var actual = 0;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest7()
+        {
+            var expected = RPN.Calculate(" !6");
+            var actual = 720;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest8()
+        {
+            var expected = RPN.Calculate(" 6 * 4");
+            var actual = 24;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest9()
+        {
+            var expected = RPN.Calculate(" 12/4");
+            var actual = 3;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest10()
+        {
+            var expected = RPN.Calculate(" 3^3");
+            var actual = 27;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest11()
+        {
+            var expected = RPN.Calculate(" 9^2");
+            var actual = 81;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest12()
+        {
+            var expected = RPN.Calculate(" 0^y");
+            var actual = 1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalculateTest13()
+        {
+            var expected = RPN.Calculate("(20/5");
+            var actual = 4;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
